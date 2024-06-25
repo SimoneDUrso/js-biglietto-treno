0dabc3db827efb92e9ba4dd0fda973cfbba775b8
SimoneDUrso <simone.durso98@gmail.com>
2024-06-25 15:21:40 +0200

    [FIX] Fixed JS file and modified the code

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ let chilometri = prompt("Inserisci i chilometri che vorresti percorrere")
 
 let sconto = 0;
 let prezzo_biglietto = 0.21 * chilometri;
-let prezzo_totale = 0;
 
 if (eta < 18) {
-    sconto = 20
+    sconto = 0.2
 }
 
 else if (eta > 65) {
-    sconto = 40
+    sconto = 0.4
 }
+
+let prezzo_finale = prezzo_biglietto - (prezzo_biglietto * sconto);
+
