6920544a8c648844f25ceb408381b391a2d550b9
SimoneDUrso <simone.durso98@gmail.com>
2024-06-25 14:43:32 +0200

    [UPD] Created variables in JS file with first if

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let eta = prompt("Inserisci la tua età");
+let chilometri = prompt("Inserisci i chilometri che vorresti percorrere")
+
+let sconto = 0;
+let prezzo_biglietto = 0.21 * chilometri;
+let prezzo_totale = 0;
+
+if (eta < 18) {
+    sconto = 20
+}
